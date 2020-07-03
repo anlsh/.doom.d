7af8cd95d2d66b01d5f66495f4e1b97018b74fde
Anish Moorthy <anlsh@protonmail.com>
2020-07-03 14:57:37 -0400

    Window keybind

--- keybindings.el
+++ keybindings.el
@@ -74,3 +74,14 @@
        :localleader
        :prefix "g"
        :desc "goto-definition" "g" #'+lookup/definition))
+(map! :leader
+    "0" #'winum-select-window-0-or-10
+    "1" #'winum-select-window-1
+    "2" #'winum-select-window-2
+    "3" #'winum-select-window-3
+    "4" #'winum-select-window-4
+    "5" #'winum-select-window-5
+    "6" #'winum-select-window-6
+    "7" #'winum-select-window-7
+    "8" #'winum-select-window-8
+    "9" #'winum-select-window-9)
