2351eb95150c933cd6a43b8223100095927c9733
Anish Moorthy <anlsh@protonmail.com>
2020-09-18 19:11:27 +0000

    Remove lispy mode, it fucks with arrow keys in EL

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
-       lispy             ; vim for lisp, for people who don't like vim
+       ;; lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
